f52847c20e34bb6a796c03338570052eee5aa65a
Adrian Trawka <ad.trawka@gmail.com>
2017-05-31 00:35:07 +0200

    Fixed headers in csv file

--- generate_data.py
+++ generate_data.py
@@ -3,6 +3,7 @@ import random
 
 init_data = datetime.date(2017, 1, 1)
 with open('clients_data.csv', 'w') as cdata:
+    cdata.write('date,clients\n')
     for day in range(130):
         date = init_data + datetime.timedelta(days=day)
         cdata.write(str(date))   
